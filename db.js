1a3febe76fc17d1f9093e7dfb76f4520e639b0b5
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-08-04 21:18:46 -0600

    display

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 function createConnection() {
     return mysql.createConnection({
-        host: 'localhost',
+        host: '156.67.73.253',
         user: 'u359191253_Emmanuel',
         password: 'GonzHern1104',
         database: 'u359191253_MynetService',
