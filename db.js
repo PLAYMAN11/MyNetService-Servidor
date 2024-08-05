e074c614d32ec27ef7d651f356e0a40d5fab0628
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-08-04 22:45:54 -0600

    Ahora si funciona

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 function createConnection() {
     return mysql.createConnection({
-        host: '156.67.73.253',
+        host: '193.203.166.204',
         user: 'u359191253_Emmanuel',
         password: 'qhmQMBu2E',
         database: 'u359191253_MynetService',
