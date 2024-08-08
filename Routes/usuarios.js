a8bf5bfbb5dedc0088ef2bd22d1ba681208e2050
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-08-07 22:23:15 -0600

    U

--- Routes/usuarios.js
+++ Routes/usuarios.js
@@ -83,7 +83,7 @@ Router.post("/MostrarDatosUsuario", async (req, res) => {
     try {
         const idUsuario = decodificarTokenParaID(req, res);
         const query = `
-            SELECT idusuario, NombreUsuario, ApellidoUsuario, CorreoUsuario, SOBREMI FROM usuarios WHERE idusuario = ?
+            SELECT idusuario, NombreUsuario, ApellidoUsuario, CorreoUsuario, SOBREMI FROM Usuarios WHERE idusuario = ?
         `;
         const [result] = await pool.query(query, [idUsuario]);
         res.status(200).json(result);
